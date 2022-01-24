703c2929d4b324c5f354c81e8538c67c58effeab
Cinthia M. Orona <devrider2021@gmail.com>
2022-01-24 14:02:54 -0300

    first commit

---
+++ src/app/service/task.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHandler} from '@angular/common/http';
+import {Observable, of} from 'rxjs'
+import {Task} from '../components/Task';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = ' http://localhost:5000/tasks'
+  //npm run server
+
+  constructor(
+    private http:HttpClient
+  ) { }
+
+  getTasks(): Observable<Task[]>{
+    return this.http.get<Task[]>(this.apiUrl)
+  }
+
+  deleteTask(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task: Task): Observable<Task>{
+    const url = `${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task)
+  }
+
+  addTask(task: Task): Observable<Task>{
+    return this.http.post<Task>(this.apiUrl, task)
+
+  }
+}
